73e2a860d5611452089ae16ef5bff149ed671920
sassykishon <sassy.kishon@icloud.com>
2024-02-07 01:33:56 +0000

    ch4c10

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunch, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
