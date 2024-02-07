93c3620f3598662a2acd44ae0d22f7a6e31216c5
sassykishon <sassy.kishon@icloud.com>
2024-02-07 01:14:22 +0000

    chapter4 commit7

--- rango/views.py
+++ rango/views.py
@@ -7,4 +7,4 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html',)
